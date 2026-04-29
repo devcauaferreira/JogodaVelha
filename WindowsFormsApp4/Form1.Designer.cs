bc7c69233e71586316359c353cfad6c7604aba73
LFSC1400 <silveiracostal0@gmail.com>
2026-04-29 08:14:46 -0300

    Subindo para o Github

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btPos9 = new System.Windows.Forms.Button();
+            this.btPos8 = new System.Windows.Forms.Button();
+            this.btPos7 = new System.Windows.Forms.Button();
+            this.btPos6 = new System.Windows.Forms.Button();
+            this.btPos4 = new System.Windows.Forms.Button();
+            this.btPos3 = new System.Windows.Forms.Button();
+            this.btPos2 = new System.Windows.Forms.Button();
+            this.btPos1 = new System.Windows.Forms.Button();
+            this.btPos5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 118F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.btPos9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btPos8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btPos7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btPos6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btPos4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btPos3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btPos2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btPos1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btPos5, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(535, 155);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.75124F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.24876F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 101F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(358, 309);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btPos9
+            // 
+            this.btPos9.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPos9.Location = new System.Drawing.Point(241, 210);
+            this.btPos9.Name = "btPos9";
+            this.btPos9.Size = new System.Drawing.Size(112, 96);
+            this.btPos9.TabIndex = 8;
+            this.btPos9.UseVisualStyleBackColor = true;
+            this.btPos9.Click += new System.EventHandler(this.Jogada_click);
+            // 
+            // btPos8
+            // 
+            this.btPos8.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPos8.Location = new System.Drawing.Point(123, 210);
+            this.btPos8.Name = "btPos8";
+            this.btPos8.Size = new System.Drawing.Size(112, 96);
+            this.btPos8.TabIndex = 7;
+            this.btPos8.UseVisualStyleBackColor = true;
+            this.btPos8.Click += new System.EventHandler(this.Jogada_click);
+            // 
+            // btPos7
+            // 
+            this.btPos7.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPos7.Location = new System.Drawing.Point(3, 210);
+            this.btPos7.Name = "btPos7";
+            this.btPos7.Size = new System.Drawing.Size(112, 96);
+            this.btPos7.TabIndex = 6;
+            this.btPos7.UseVisualStyleBackColor = true;
+            this.btPos7.Click += new System.EventHandler(this.Jogada_click);
+            // 
+            // btPos6
+            // 
+            this.btPos6.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPos6.Location = new System.Drawing.Point(241, 106);
+            this.btPos6.Name = "btPos6";
+            this.btPos6.Size = new System.Drawing.Size(112, 97);
+            this.btPos6.TabIndex = 5;
+            this.btPos6.UseVisualStyleBackColor = true;
+            this.btPos6.Click += new System.EventHandler(this.Jogada_click);
+            // 
+            // btPos4
+            // 
+            this.btPos4.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPos4.Location = new System.Drawing.Point(3, 106);
+            this.btPos4.Name = "btPos4";
+            this.btPos4.Size = new System.Drawing.Size(112, 97);
+            this.btPos4.TabIndex = 3;
+            this.btPos4.UseVisualStyleBackColor = true;
+            this.btPos4.Click += new System.EventHandler(this.Jogada_click);
+            // 
+            // btPos3
+            // 
+            this.btPos3.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPos3.Location = new System.Drawing.Point(241, 3);
+            this.btPos3.Name = "btPos3";
+            this.btPos3.Size = new System.Drawing.Size(112, 97);
+            this.btPos3.TabIndex = 2;
+            this.btPos3.UseVisualStyleBackColor = true;
+            this.btPos3.Click += new System.EventHandler(this.Jogada_click);
+            // 
+            // btPos2
+            // 
+            this.btPos2.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPos2.Location = new System.Drawing.Point(123, 3);
+            this.btPos2.Name = "btPos2";
+            this.btPos2.Size = new System.Drawing.Size(112, 97);
+            this.btPos2.TabIndex = 1;
+            this.btPos2.UseVisualStyleBackColor = true;
+            this.btPos2.Click += new System.EventHandler(this.Jogada_click);
+            // 
+            // btPos1
+            // 
+            this.btPos1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPos1.Location = new System.Drawing.Point(3, 3);
+            this.btPos1.Name = "btPos1";
+            this.btPos1.Size = new System.Drawing.Size(114, 97);
+            this.btPos1.TabIndex = 0;
+            this.btPos1.UseVisualStyleBackColor = true;
+            this.btPos1.Click += new System.EventHandler(this.Jogada_click);
+            // 
+            // btPos5
+            // 
+            this.btPos5.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPos5.Location = new System.Drawing.Point(123, 106);
+            this.btPos5.Name = "btPos5";
+            this.btPos5.Size = new System.Drawing.Size(112, 97);
+            this.btPos5.TabIndex = 4;
+            this.btPos5.UseVisualStyleBackColor = true;
+            this.btPos5.Click += new System.EventHandler(this.Jogada_click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(588, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Jogo da Velha";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.ClientSize = new System.Drawing.Size(1449, 655);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btPos2;
+        private System.Windows.Forms.Button btPos1;
+        private System.Windows.Forms.Button btPos9;
+        private System.Windows.Forms.Button btPos8;
+        private System.Windows.Forms.Button btPos7;
+        private System.Windows.Forms.Button btPos6;
+        private System.Windows.Forms.Button btPos5;
+        private System.Windows.Forms.Button btPos4;
+        private System.Windows.Forms.Button btPos3;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        bool jogadorX=true;
+        int jogadas = 0;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Jogada_click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.Text == "")
+            {
+                btn.Text = jogadorX ? "X" : "O";
+                jogadorX = !jogadorX;
+                jogadas++;
+
+                if (verificaTemVencedor())
+                {
+                    string vencedor = jogadorX ? "O" : "X";
+                    MessageBox.Show("JOGADOR " + vencedor + " venceu");
+                    return;
+                }
+
+                if(jogadas==9)
+                    {
+                        MessageBox.Show("Deu Velha!");
+                    return;
+                    }
+            }
+           
+        }
+         private bool verificaTemVencedor()
+        {
+            if (
+              (btPos1.Text == btPos2.Text && btPos2.Text == btPos3.Text && btPos1.Text != "") ||
+              (btPos4.Text == btPos5.Text && btPos5.Text == btPos6.Text && btPos4.Text != "") ||
+              (btPos7.Text == btPos8.Text && btPos8.Text == btPos9.Text && btPos7.Text != "") ||
+
+              (btPos1.Text == btPos4.Text && btPos4.Text == btPos7.Text && btPos1.Text != "") ||
+              (btPos2.Text == btPos5.Text && btPos5.Text == btPos8.Text && btPos2.Text != "") ||
+              (btPos3.Text == btPos6.Text && btPos6.Text == btPos9.Text && btPos3.Text != "") ||
+
+              (btPos1.Text == btPos5.Text && btPos5.Text == btPos9.Text && btPos1.Text != "") ||
+              (btPos3.Text == btPos5.Text && btPos5.Text == btPos7.Text && btPos3.Text != "") 
+
+              ) 
+                    
+            {
+                return true;
+                
+            }
+                return false;
+        }
+      
+
+
+
+
+    }
+}
